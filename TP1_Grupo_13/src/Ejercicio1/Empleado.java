a90e85a3634bee81b4249812df2e38b064edc259
Karina Rivera <karina.rivera@hotmail.es>
2024-03-29 15:51:52 -0300

    agrego encapsulamiento, getters y setters, comienzo con constructor

--- TP1_Grupo_13/src/Ejercicio1/Empleado.java
+++ TP1_Grupo_13/src/Ejercicio1/Empleado.java
@@ -1,7 +1,37 @@
 package Ejercicio1;
 
 public class Empleado {
-	int id;
-	String nombre;
-	int edad;
+	private int id;
+	private String nombre;
+	private int edad;
+	
+	//constructor
+	
+	public Empleado() {
+		id=1000;
+		nombre= "sin nombre";
+		edad=0;
+	}
+	
+	//getters y setters
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
 }
+
+
