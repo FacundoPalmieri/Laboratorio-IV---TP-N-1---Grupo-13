1fdefbe22f071ce6aafcc01df71138e135eea42f
LucasJavierGutierrez <64700583+LucasJavierGutierrez@users.noreply.github.com>
2024-04-01 18:55:25 -0300

    Revision
    Agrego la carga de los 5 empleados y los muestro

--- TP1_Grupo_13/src/Ejercicio1/Principal.java
+++ TP1_Grupo_13/src/Ejercicio1/Principal.java
@@ -1,10 +1,41 @@
 package Ejercicio1;
 
+
+
 public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
+		Empleado emp = new Empleado();
+		
+		emp.setNombre("Carlos");
+		emp.setEdad(25);
+		
+		Empleado emp1 = new Empleado("Ernesto",45);	
+		
+		Empleado emp2 = new Empleado();
+		
+		emp2.setNombre("Lucas");
+		emp2.setEdad(20);
+		
+		Empleado emp3 = new Empleado();
+		
+		emp3.setNombre("Juan");
+		emp3.setEdad(35);
+		
+		Empleado emp4 = new Empleado("Pedro",29);
+		
+		System.out.println(emp.toString());
+		System.out.println(emp1.toString());
+		System.out.println(emp2.toString());
+		System.out.println(emp3.toString());
+		System.out.println(emp4.toString());
+
 	}
 
+
+
+	
 }
